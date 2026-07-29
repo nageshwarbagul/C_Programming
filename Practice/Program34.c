72371644abde7c8147225f3404d3a333bd365c40
Nageshwar Bagul <nageshwarbagul08@gmail.com>
2026-07-30 00:41:12 +0530

    program to print numbers from N to 0

---
+++ Practice/Program34.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void Dispaly(int iNo)
+{
+    int iCnt = 0;
+
+    // Filter
+    if(iNo < 0)
+    {
+        printf("Invalid Input");
+        return;
+    }
+    for(iCnt = iNo; iCnt >= 0; iCnt--)
+    {
+        printf("%d\n",iCnt);
+    }
+}
+
+int main()
+{
+    int iVlaue = 0;
+
+    printf("Enter fequency :\n");
+    scanf("%d", &iVlaue);
+
+    Dispaly(iVlaue);
+    return 0;
+}
