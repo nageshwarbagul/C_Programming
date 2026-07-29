51c87a88944b0988ab3061d0f46e4ae740babec9
Nageshwar Bagul <nageshwarbagul08@gmail.com>
2026-07-30 00:21:33 +0530

    program to print message based on user-defined frequency

---
+++ Practice/Program27.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+    int iVlaue = 0;
+
+    printf("Enter fequency :\n");
+    scanf("%d", &iVlaue);
+
+    // Iteration
+    for(iCnt = 1; iCnt <= iVlaue; iCnt++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+
+    return 0;
+}
