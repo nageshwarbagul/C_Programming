f23316e1459f56d5d79b85aad2647e2b65be148c
Nageshwar Bagul <nageshwarbagul08@gmail.com>
2026-07-30 00:23:25 +0530

    function to print message multiple times

---
+++ Practice/Program28.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void Dispaly(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+}
+
+int main()
+{
+    int iVlaue = 0;
+
+    printf("Enter fequency :\n");
+    scanf("%d", &iVlaue);
+
+    Dispaly(iVlaue);
+    return 0;
+}
