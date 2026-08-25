055cd5ffae24b572d1efcbb6055a25d0d00760bf
Nageshwar Bagul <nageshwarbagul08@gmail.com>
2026-08-25 23:49:09 +0530

    Add basic string handling programs in C

---
+++ Practice/Program272.c
@@ -0,0 +1,26 @@
+    #include<stdio.h>
+
+    int main()
+    {
+        char *str = "Ganesh";  // char ch [] = "Ganesh";
+
+        printf("%c\n",*str);
+        str++;
+
+        printf("%c\n",*str);
+        str++;
+
+        printf("%c\n",*str);
+        str++;
+
+        printf("%c\n",*str);
+        str++;
+
+        printf("%c\n",*str);
+        str++;
+
+        printf("%c\n",*str);
+        str++;
+        
+        return 0;
+    }
