8ac6a7264c031e2f31e0df460e2b02ada8996b39
Nageshwar Bagul <nageshwarbagul08@gmail.com>
2026-08-30 23:59:50 +0530

    String Programs

---
+++ Practice/Program294.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void Upadate( char *str)
+{
+    while(*str != '\0')
+    {
+        if(*str == 'l')
+        {
+            *str = '-';
+        }
+        str++;
+    }
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",Arr);
+
+    Upadate(Arr);
+
+    printf("Upadate String is : %s\n",Arr);
+    
+    return 0;
+}
